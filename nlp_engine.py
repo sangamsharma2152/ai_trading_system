db5766c73f3442c69882112a46147f92a4b32b8a
Sangam Sharma <sangamsharma2152@gmail.com>
2026-03-20 01:53:34 +0530

    Update nlp_engine.py

--- nlp_engine.py
+++ nlp_engine.py
@@ -1,14 +1,31 @@
-import spacy
+import re
 
-# Load model once
-nlp = spacy.load("en_core_web_sm")
+# Predefined location keywords (expandable)
+LOCATION_KEYWORDS = {
+    "usa": (37.0902, -95.7129),
+    "america": (37.0902, -95.7129),
+    "china": (35.8617, 104.1954),
+    "russia": (61.5240, 105.3188),
+    "ukraine": (48.3794, 31.1656),
+    "india": (20.5937, 78.9629),
+    "europe": (54.5260, 15.2551),
+    "middle east": (25.276987, 55.296249),
+    "iran": (32.4279, 53.6880),
+    "iraq": (33.2232, 43.6793),
+    "israel": (31.0461, 34.8516),
+    "saudi": (23.8859, 45.0792),
+}
 
 def extract_locations(text):
-    doc = nlp(text)
-    locations = []
+    text = text.lower()
+    found_locations = []
 
-    for ent in doc.ents:
-        if ent.label_ in ["GPE", "LOC"]:
-            locations.append(ent.text)
+    for key in LOCATION_KEYWORDS:
+        if re.search(rf"\b{key}\b", text):
+            found_locations.append(key)
 
-    return locations
+    return found_locations
+
+
+def get_coordinates(location):
+    return LOCATION_KEYWORDS.get(location, (None, None))
