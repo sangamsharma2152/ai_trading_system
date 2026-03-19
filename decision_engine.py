f2e4b90528c6eeace605252bfebce45abb0e22ee
Sangam Sharma <sangamsharma2152@gmail.com>
2026-03-20 00:13:41 +0530

    Create decision_engine.py

---
+++ decision_engine.py
@@ -0,0 +1,10 @@
+def generate_decision(prediction, confidence):
+    decisions = {}
+
+    for asset, action in prediction.items():
+        decisions[asset] = {
+            "action": action,
+            "confidence": round(abs(confidence) * 100, 2)
+        }
+
+    return decisions
