8eb95efc3925ff2371ec45899a147f208404cfbc
Sangam Sharma <sangamsharma2152@gmail.com>
2026-03-20 01:23:07 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,17 +1,17 @@
 from transformers import pipeline
 
-# Load once
 sentiment_model = pipeline("sentiment-analysis")
 
 def analyze_sentiment(news_list):
     results = []
 
     for news in news_list:
-        result = sentiment_model(news)[0]
+        result = sentiment_model(news["title"])[0]
         results.append({
-            "text": news,
+            "text": news["title"],
             "label": result["label"],
-            "score": result["score"]
+            "score": result["score"],
+            "source": news["source"]
         })
 
     return results
