886be7d9059c66cac68441dbfcfcc4eb7f68e9ba
Sangam Sharma <sangamsharma2152@gmail.com>
2026-03-20 01:25:00 +0530

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -6,7 +6,7 @@ def get_prices():
     oil = yf.download("CL=F", period="1d", interval="1m")
 
     return {
-        "gold": gold["Close"].iloc[-1],
-        "silver": silver["Close"].iloc[-1],
-        "oil": oil["Close"].iloc[-1]
+        "gold": float(gold["Close"].iloc[-1]),
+        "silver": float(silver["Close"].iloc[-1]),
+        "oil": float(oil["Close"].iloc[-1])
     }
