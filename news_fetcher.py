46d8c7063d9c3a29bc76dbf82c4eebcc8df59256
Sangam Sharma <sangamsharma2152@gmail.com>
2026-03-20 01:22:48 +0530

    Update news_fetcher.py

--- news_fetcher.py
+++ news_fetcher.py
@@ -1,13 +1,17 @@
 import requests
-from config import NEWS_API_KEY
+from config import NEWS_API_KEY, NEWS_LIMIT
 
 def get_news():
-    url = f"https://newsapi.org/v2/top-headlines?category=business&language=en&apiKey={NEWS_API_KEY}"
+    url = f"https://newsapi.org/v2/everything?q=gold OR oil OR inflation OR war OR economy&language=en&sortBy=publishedAt&apiKey={NEWS_API_KEY}"
+    
     response = requests.get(url)
     data = response.json()
 
     articles = []
-    for article in data["articles"][:10]:
-        articles.append(article["title"])
+    for article in data["articles"][:NEWS_LIMIT]:
+        articles.append({
+            "title": article["title"],
+            "source": article["source"]["name"]
+        })
 
     return articles
